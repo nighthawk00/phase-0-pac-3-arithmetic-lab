f9717a281468fd00f49d8674344e880bfde8ebb6
Kevin <kevinlopez3292@gmail.com>
2022-11-14 10:20:13 -0500

    commit tree 082d9e86dc19a1a13908a14090ba990dd1bba157

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+ return a + b;
+}
+function subtract(a, b) {
+ return a - b;
+}
+function multiply(a, b) {
+ return a * b;
+}
+function divide(a, b) {
+ return a / b;
+}
+function increment(n) {
+ return ++n;
+}
+function decrement(n) {
+ return --n;
+}
+function makeInt(n) {
+ parseInt(n, 10);
+}
+function preserveDecimal(n) {
+ parseFloat(n);
+}
